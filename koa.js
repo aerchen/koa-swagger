0cb30fde6745c3d53f942895f88275ba985b0e21
chenjiangang <15012937056@163.com>
2018-09-13 16:37:27 +0800

    'enable'

---
+++ koa.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var fs = require('fs'),
+    path = require('path'),
+    koa = require('koa');
+
+var app = new koa();
+var jsyaml = require('js-yaml');
+
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync(path.join(__dirname,'api/swagger.yaml'), 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+const swaggerUi = function (options) {
+  var swaggerUi = require('./koa-swagger-ui');
+  var suArgs = [swaggerDoc];
+
+  suArgs.push(options || {});
+
+  return swaggerUi.apply(undefined, suArgs);
+};
+
+app.use(async (ctx, next) => {
+  await next().catch(console.error.bind(null, 'errorHandler'));
+});
+
+// Serve the Swagger documents and Swagger UI
+app.use(swaggerUi({swaggerUi: 'swagger'}));
+
+app.use(async (ctx, next) => {
+  ctx.body = ctx.request;
+});
+
+var serverPort = 8090;
+// Start the server
+app.listen(serverPort, function () {
+  console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  console.log('Swagger-ui is available on http://localhost:%d/docs/', serverPort);
+});
+
+
